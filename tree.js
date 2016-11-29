ec982d72e926b7d70b8106bc644d893ebd5f45b5
Slobodan Milovanovic <bluesboba@gmail.com>
2016-11-29 16:06:05 +0100

    Beautify files.

--- tree.js
+++ tree.js
@@ -10,9 +10,9 @@ var minBoxWidth = 170,
         bottom: 16,
         left: 16
     },
-    data,   // set after main code comment
-    svg,    // set after main code comment
-    width,  // set after main code comment
+    data, // set after main code comment
+    svg, // set after main code comment
+    width, // set after main code comment
     height; // set after main code comment
 
 // test layout
@@ -24,7 +24,7 @@ var numLvls = 0,
     boxWidth = [];
 
 var diagonal = d3.svg.diagonal()
-    .projection(function (d) {
+    .projection(function(d) {
         "use strict";
         return [d.y, d.x];
     });
@@ -41,29 +41,29 @@ function find(text) {
 }
 
 // Bring the current selection to the front of drawing.
-// This is neccessary when there are to many links 
+// This is neccessary when there are to many links
 // and the highlighted link is behind all of them.
 d3.selection.prototype.moveToFront = function() {
-  return this.each(function(){
-    this.parentNode.appendChild(this);
-  });
+    return this.each(function() {
+        this.parentNode.appendChild(this);
+    });
 };
 
 // Reverse function to moveToFront
-d3.selection.prototype.moveToBack = function() { 
-    return this.each(function() { 
-        var firstChild = this.parentNode.firstChild; 
-        if (firstChild) { 
-            this.parentNode.insertBefore(this, firstChild); 
-        } 
-    }); 
+d3.selection.prototype.moveToBack = function() {
+    return this.each(function() {
+        var firstChild = this.parentNode.firstChild;
+        if (firstChild) {
+            this.parentNode.insertBefore(this, firstChild);
+        }
+    });
 };
 
-function switch_state (d, stat) {
-    if (stat){
+function switch_state(d, stat) {
+    if (stat) {
         d3.select("#" + d.id).moveToFront().classed("activelink", stat); // change link color
         d3.select("#" + d.id).moveToFront().classed("link", !stat); // change link color
-    }else{
+    } else {
         d3.select("#" + d.id).moveToBack().classed("activelink", stat); // change link color
         d3.select("#" + d.id).moveToBack().classed("link", !stat); // change link color
     }
@@ -72,31 +72,31 @@ function switch_state (d, stat) {
 function mouse_action(val, stat, direction) {
     "use strict";
     d3.select("#" + val.id).classed("active", stat);
-    if(val.url)
+    if (val.url)
         d3.select("#" + val.id).classed("witurl", !stat);
 
-    links.forEach(function (d) {
-        // This is used to reduce the amount of repetition in the code. 
+    links.forEach(function(d) {
+        // This is used to reduce the amount of repetition in the code.
         // For previous implementation see git log.
         var st = [d.source, d.target];
-        for (var i = 0; i < 2; i++){
+        for (var i = 0; i < 2; i++) {
             if (direction == "root") {
                 if (st[i].id === val.id) {
                     switch_state(d, stat);
-                    if (st[(i+1)%2].lvl < val.lvl)
-                        mouse_action(st[(i+1)%2], stat, "left");
-                    else if (st[(i+1)%2].lvl > val.lvl)
-                        mouse_action(st[(i+1)%2], stat, "right");
+                    if (st[(i + 1) % 2].lvl < val.lvl)
+                        mouse_action(st[(i + 1) % 2], stat, "left");
+                    else if (st[(i + 1) % 2].lvl > val.lvl)
+                        mouse_action(st[(i + 1) % 2], stat, "right");
                 }
-            }else if (direction == "left") {
-                if (st[i].id === val.id && st[(i+1)%2].lvl < val.lvl) {
+            } else if (direction == "left") {
+                if (st[i].id === val.id && st[(i + 1) % 2].lvl < val.lvl) {
                     switch_state(d, stat);
-                    mouse_action(st[(i+1)%2], stat, direction);
+                    mouse_action(st[(i + 1) % 2], stat, direction);
                 }
-            }else if (direction == "right") {
-                if (st[i].id === val.id && st[(i+1)%2].lvl > val.lvl) {
+            } else if (direction == "right") {
+                if (st[i].id === val.id && st[(i + 1) % 2].lvl > val.lvl) {
                     switch_state(d, stat);
-                    mouse_action(st[(i+1)%2], stat, direction);
+                    mouse_action(st[(i + 1) % 2], stat, direction);
                 }
             }
         }
@@ -105,45 +105,45 @@ function mouse_action(val, stat, direction) {
 
 var tip = d3.tip()
     .attr('class', 'd3-tip')
-//    .direction('e')
+    //    .direction('e')
     .offset([-10, 0])
     .html(function(d) {
         var str = "";
-        if (d.url && d.dsc){
+        if (d.url && d.dsc) {
             str = "<font color=\"red\">Click to open website</font><br/><br/>";
             str += d.dsc;
-        }else if(d.url)
+        } else if (d.url)
             str = "<font color=\"red\">Click to open website</font>";
         else
             str = d.dsc;
-            
+
         return str;
     })
 
 function renderRelationshipGraph(data) {
     "use strict";
 
-    data.Nodes.forEach(function (d, i) {
+    data.Nodes.forEach(function(d, i) {
         var bv = 0;
         for (var j = 0; j < d.lvl; j++)
             bv += boxWidth[j] + gap.width;
-        d.x = margin.left + bv;//d.lvl * (boxWidth + gap.width);
+        d.x = margin.left + bv; //d.lvl * (boxWidth + gap.width);
         d.y = topMarginLvl[d.lvl] + (boxHeight + gap.height) * count[d.lvl];
         d.id = "n" + i;
         count[d.lvl] += 1;
         Nodes.push(d);
     });
 
-    data.links.forEach(function (d) {
+    data.links.forEach(function(d) {
         // If there is no node with the mentioned id, omit the link
-        if( find(d.source) != null && find(d.target) != null ){
+        if (find(d.source) != null && find(d.target) != null) {
             links.push({
                 source: find(d.source),
                 target: find(d.target),
                 id: "l" + find(d.source).id + find(d.target).id
             });
-        }else{
-            console.warn("One of the IDs \'" +d.source+ "\' or \'" + d.target + "\' do not point to a node");
+        } else {
+            console.warn("One of the IDs \'" + d.source + "\' or \'" + d.target + "\' do not point to a node");
         }
     });
 
@@ -158,37 +158,55 @@ function renderRelationshipGraph(data) {
         .attr("class", "unit");
 
     node.append("rect")
-        .attr("x", function (d) { return d.x; })
-        .attr("y", function (d) { return d.y; })
-        .attr("id", function (d) { return d.id; })
-        .attr("width", function (d) { return boxWidth[d.lvl];})
+        .attr("x", function(d) {
+            return d.x;
+        })
+        .attr("y", function(d) {
+            return d.y;
+        })
+        .attr("id", function(d) {
+            return d.id;
+        })
+        .attr("width", function(d) {
+            return boxWidth[d.lvl];
+        })
         .attr("height", boxHeight)
-        .attr("class", function (d) { return d.url? "node witurl":"node";})
+        .attr("class", function(d) {
+            return d.url ? "node witurl" : "node";
+        })
         .attr("rx", 6)
         .attr("ry", 6)
-        .on("mouseover", function () {
+        .on("mouseover", function() {
             var d = d3.select(this).datum();
             mouse_action(d, true, "root");
-            if(d.url || d.dsc) tip.show(d)
+            if (d.url || d.dsc) tip.show(d)
         })
-        .on("mouseout", function () {
+        .on("mouseout", function() {
             var d = d3.select(this).datum();
             mouse_action(d, false, "root");
-            if(d.url || d.dsc) tip.hide(d)
+            if (d.url || d.dsc) tip.hide(d)
         })
-        .on("click", function(d) { if(d.url) window.open(d.url); }); // If a url is available, put a click event
+        .on("click", function(d) {
+            if (d.url) window.open(d.url);
+        }); // If a url is available, put a click event
 
     node.append("text")
         .attr("class", "label")
-        .attr("x", function (d) { return d.x + 14; })
-        .attr("y", function (d) { return d.y + 15; })
-        .text(function (d) { return d.text; });
+        .attr("x", function(d) {
+            return d.x + 14;
+        })
+        .attr("y", function(d) {
+            return d.y + 15;
+        })
+        .text(function(d) {
+            return d.text;
+        });
 
-    links.forEach(function (li) {
+    links.forEach(function(li) {
         svg.append("path", "g")
             .attr("class", "link")
             .attr("id", li.id)
-            .attr("d", function () {
+            .attr("d", function() {
                 var oTarget = {
                     x: li.target.y + 0.5 * boxHeight,
                     y: li.target.x
@@ -197,7 +215,7 @@ function renderRelationshipGraph(data) {
                     x: li.source.y + 0.5 * boxHeight,
                     y: li.source.x
                 };
-                
+
                 if (oSource.y < oTarget.y) {
                     oSource.y += boxWidth[li.source.lvl];
                 } else {
@@ -212,8 +230,8 @@ function renderRelationshipGraph(data) {
 }
 
 // This function computes the size of the box needed to fit the node text.
-window.getWidthOfText = function(txt){
-  var classes = document.styleSheets[0].rules || document.styleSheets[0].cssRules;
+window.getWidthOfText = function(txt) {
+    var classes = document.styleSheets[0].rules || document.styleSheets[0].cssRules;
     for (var x = 0; x < classes.length; x++) {
         if (classes[x].selectorText === ".label") {
             // Create dummy span
@@ -235,48 +253,48 @@ window.getWidthOfText = function(txt){
 
 // M A I N   C O D E
 
-d3.json("flare.json", function (json) {
+d3.json("flare.json", function(json) {
     "use strict";
     data = json;
-    
-    data.Nodes.forEach(function (d) {
+
+    data.Nodes.forEach(function(d) {
         count[d.lvl] = 0;
         topMarginLvl[d.lvl] = 0;
         boxWidth[d.lvl] = minBoxWidth;
     });
     numLvls = count.length;
-    
+
     // Find the number of items in each level and the size of each level.
-    data.Nodes.forEach(function (d) {
+    data.Nodes.forEach(function(d) {
         topMarginLvl[d.lvl] += 1;
-        if(getWidthOfText(d.text) > boxWidth[d.lvl])
+        if (getWidthOfText(d.text) > boxWidth[d.lvl])
             boxWidth[d.lvl] = getWidthOfText(d.text);
     });
-    
+
     width = 0;
-    for(var i = 0; i < boxWidth.length-1; i++){
+    for (var i = 0; i < boxWidth.length - 1; i++) {
         width += boxWidth[i] + gap.width;
     }
-    
-    width += margin.left + margin.right + boxWidth[boxWidth.length-1];
-    
+
+    width += margin.left + margin.right + boxWidth[boxWidth.length - 1];
+
     var largest = Math.max.apply(Math, topMarginLvl);
-    
-    // Update height and width to have all the nodes in the image and also have 
+
+    // Update height and width to have all the nodes in the image and also have
     // the smallest image possible.
-    height = margin.top + margin.bottom + largest*boxHeight + (largest - 1) * gap.height;
-//    width  = margin.left + margin.right + topMarginLvl.length*boxWidth + (topMarginLvl.length - 1)*gap.width;
-    
-    for (var i = 0; i < topMarginLvl.length; i++){
-        topMarginLvl[i] = (height - topMarginLvl[i]*boxHeight - (topMarginLvl[i]-1)*gap.height)/2;
+    height = margin.top + margin.bottom + largest * boxHeight + (largest - 1) * gap.height;
+    //    width  = margin.left + margin.right + topMarginLvl.length*boxWidth + (topMarginLvl.length - 1)*gap.width;
+
+    for (var i = 0; i < topMarginLvl.length; i++) {
+        topMarginLvl[i] = (height - topMarginLvl[i] * boxHeight - (topMarginLvl[i] - 1) * gap.height) / 2;
     }
-    
+
     svg = d3.select("#tree").append("svg")
-                .attr("width", width)
-                .attr("height", height)
-                .append("g");
-    
+        .attr("width", width)
+        .attr("height", height)
+        .append("g");
+
     svg.call(tip);
-    
+
     renderRelationshipGraph(data);
 });
